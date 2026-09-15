ff173f8a4e7392c8ce83c66ad6769e5d1942f3b9
Felix Johansson <felix.william1993@gmail.com>
2026-09-15 15:09:41 +0200

    Added Goblin Enemy

--- Assets/Scripts/Managers/EnemySpawner.cs
+++ Assets/Scripts/Managers/EnemySpawner.cs
@@ -9,6 +9,7 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private Timer timer;
     //[SerializeField] Transform spawnPos;
     [SerializeField] private List<GameObject> enemyTypes = new List<GameObject>();
+    [SerializeField] private List<GameObject> spawnPoints = new List<GameObject>();
 
     private bool canSpawn = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -24,30 +25,46 @@ public class EnemySpawner : MonoBehaviour
         {
             canSpawn = true;
         }
-        SpawnEnemy(0, transform.position); // Example: spawn the first enemy type at the spawner's position  
+        SpawnEnemy(); // Example: spawn the first enemy type at the spawner's position  
     }
 
-    private void SpawnEnemy(int enemyTypeIndex, Vector2 position)
+    private void SpawnEnemy()
     {
-        if (enemyTypeIndex < 0 || enemyTypeIndex >= enemyTypes.Count)
-        {
-            Debug.LogError("Invalid enemy type index: " + enemyTypeIndex);
-            return;
-        }
+        //if (enemyTypeIndex < 0 || enemyTypeIndex >= enemyTypes.Count)
+        //{
+        //    Debug.LogError("Invalid enemy type index: " + enemyTypeIndex);
+        //    return;
+        //}
         
-        
-        int amountToSpawn = waveManager.AmountOfEnemies; // Assuming you have a WaveManager instance to get the amount of enemies to spawn
-
         if ((timer.Seconds % 10 == 0 || timer.Seconds == 0) && canSpawn) // Spawn enemies every 10 seconds
         {
+            int amountToSpawn = waveManager.AmountOfEnemies; // Assuming you have a WaveManager instance to get the amount of enemies to spawn
+            int _rndmIndex = GenerateSpawnPoint();
+            Debug.Log(_rndmIndex);
+            int enemyTypeIndex = GenerateEnemyType();
+            Debug.Log(enemyTypeIndex);
+
             canSpawn = false;
             // Spawn enemies
             for (int i = 0; i < amountToSpawn; i++)
             {
                 var enemy = Instantiate(enemyTypes[enemyTypeIndex]);
-                enemy.transform.position = position;
+                enemy.transform.position = spawnPoints[_rndmIndex].transform.position;
             }
-        }
-        
+        } 
+    }
+
+    private int GenerateSpawnPoint()
+    {
+        int maxSpawnPoints = spawnPoints.Count;
+        int rndmIndex = Random.Range(0, maxSpawnPoints);
+        return rndmIndex;
+    }
+
+    private int GenerateEnemyType()
+    {
+        int maxEnemyTypes = enemyTypes.Count;
+        int rndmIndex = Random.Range(0, maxEnemyTypes);
+        return rndmIndex;
     }
 }
