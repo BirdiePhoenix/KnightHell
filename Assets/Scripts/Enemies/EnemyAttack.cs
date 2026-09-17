1ed9a115e4872491ed22376e553cec3476b5daa7
Felix Johansson <felix.william1993@gmail.com>
2026-09-17 12:13:16 +0200

    Working on EnemyAttack

--- Assets/Scripts/Enemies/EnemyAttack.cs
+++ Assets/Scripts/Enemies/EnemyAttack.cs
@@ -12,6 +12,15 @@ public class EnemyAttack : MonoBehaviour
         player = GameObject.FindGameObjectWithTag("Player");
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision");
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Hit!");
+        }
+    }
+
     public IEnumerator Attack()
     {
         yield return new WaitForSeconds(enemyStats.attackSpeed);
--- Assets/Scripts/Enemies/EnemyMovement.cs
+++ Assets/Scripts/Enemies/EnemyMovement.cs
@@ -23,17 +23,17 @@ public class EnemyMovement : MonoBehaviour
     {
         distance = Vector2.Distance(player.transform.position, transform.position);
         Vector2 lookDirection = (player.transform.position - transform.position).normalized;
+        enemyRb.MovePosition(enemyRb.position + lookDirection * enemyStats.movementSpeed * Time.fixedDeltaTime);
         //enemyRb.AddForce(lookDirection * enemySpeed);
-        Debug.Log(distance);
         //Checks if the distance between the player and the enemy is greater than the stopping distance
-        if (distance > stoppingDistance)
-        {
-            enemyRb.MovePosition(enemyRb.position + lookDirection * enemyStats.movementSpeed * Time.fixedDeltaTime);
-        }
-        else
-        {
-            enemyAttack.Attack();
-        }
+        //if (distance > stoppingDistance)
+        //{
+        //    enemyRb.MovePosition(enemyRb.position + lookDirection * enemyStats.movementSpeed * Time.fixedDeltaTime);
+        //}
+        //else
+        //{
+        //    enemyAttack.Attack();
+        //}
         
     }
 
